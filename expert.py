3fefdcb2d44c5a8b9db91cfb56c2911a4011e8c4
pasha <paul@drteam.rocks>
2024-08-15 13:45:08 +0300

    Small brushup and refactoring

--- expert.py
+++ expert.py
@@ -1,16 +1,16 @@
 from jinja2 import Template
 from openai import OpenAI
-from settings import OPENAI_API_KEY, OPENAI_BASE_URL
+from settings import OPENAI_API_KEY, OPENAI_BASE_URL, MAX_TOKENS_OUTPUT, PROMPT_TEMPLATE
 
 
 class Expert:
     client = None
 
     def __init__(self, model, base_url=None, api_key=None, prompt_template=None):
-        self.prompt_template = prompt_template
         self.model = model
         self.base_url = base_url or OPENAI_BASE_URL
         self.api_key = api_key or OPENAI_API_KEY
+        self.prompt_template = prompt_template or PROMPT_TEMPLATE
 
         if self.client is None:
             self.init_client()
@@ -18,24 +18,27 @@ class Expert:
     def init_client(self):
         self.client = OpenAI(api_key=self.api_key, base_url=self.base_url)
 
-    def rate_element(self, data: str) -> int:
-        prompt = Template(self.prompt_template).render(context=data)
+    def rate_sample(self, sample: str) -> int | None:
+        prompt = Template(self.prompt_template).render(context=sample)
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
         ]
-
-        response = self.client.chat.completions.create(model=self.model, messages=messages, temperature=0, n=1)
+        response = self.client.chat.completions.create(
+            model=self.model, messages=messages,
+            temperature=0, n=1, max_tokens=MAX_TOKENS_OUTPUT
+        )
 
         # Extract the response text and parse it as an integer rating from 1 to 5
         response_text = response.choices[0].message.content
         try:
-            rating = int(response_text.strip())
+            rating = int(''.join(filter(str.isdigit, response_text)))
         except ValueError:
-            raise ValueError("Invalid rating value in response: {}".format(response_text))
+            print("Invalid rating value in response: {}".format(response_text))
+            return None
 
         # Ensure the rating is within the valid range [1, 5]
-        if rating < 1 or rating > 5:
-            raise ValueError("Rating out of range: {}".format(rating))
+        if rating < 1: rating = 1
+        if rating > 5: rating = 5
 
         return rating
--- settings.py
+++ settings.py
@@ -5,5 +5,10 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL", "https://api.openai.com/v1")
+
+PROMPT_TEMPLATE = os.getenv("PROMPT_TEMPLATE")
+
+MAX_TOKENS_INPUT = int(os.getenv("MAX_TOKENS_INPUT", 4000))
+MAX_TOKENS_OUTPUT = int(os.getenv("MAX_TOKENS_OUTPUT", 10))
